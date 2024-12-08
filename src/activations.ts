448a0f0e682f9b3c333e2aaf4e8cc8803b2196f3
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2024-12-08 19:49:53 +0100

    [#2] Hook up into the file-about-to-be-saved event

--- src/activations.ts
+++ src/activations.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import {TextDocumentWillSaveEvent} from "vscode";
 import * as console from "node:console";
 
 // Currently we are manually keeping track of the set of open files,
@@ -22,6 +23,21 @@ export function handleFileClosed(doc: vscode.TextDocument) {
 
     console.log("File closed: " + doc.fileName);
     openTextFiles.delete(doc.fileName);
+}
+
+// As per the documentation, this is not 100% guaranteed to be called
+// All listeners share the same 1.5s time budget, and also they are called sequentially, so if one of the listeners
+// before ours goes over the budget, then our listener *will* not be called. Offending listeners are ignored after
+// 3 violations, so eventually our listener will be called.
+export function handleBeforeFileSaved(event: TextDocumentWillSaveEvent) {
+    if (!shouldProcessFile(event.document)) {
+        return;
+    }
+
+    event.waitUntil(Promise.resolve([
+        // TODO: Notify the server about changes
+        vscode.TextEdit.insert(new vscode.Position(0, 0), `// This file was modified at tea-vscode extension at ${new Date().toISOString()}\n`)
+    ]));
 }
 
 function shouldProcessFile(doc: vscode.TextDocument): boolean {
